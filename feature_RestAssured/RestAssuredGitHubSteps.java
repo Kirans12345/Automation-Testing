5dfdebd68ec39ef43702d3d43db5492e908cb9fc
Kiran S <126252531+Kirans12345@users.noreply.github.com>
2023-12-20 16:46:02 +0530

    RestAssured

---
+++ feature_RestAssured/RestAssuredGitHubSteps.java
@@ -0,0 +1,45 @@
+	package feature_RestAssured;
+
+	import io.cucumber.java.en.Given;
+	import io.cucumber.java.en.Then;
+	import io.cucumber.java.en.When;
+	import io.restassured.response.Response;
+	import static io.restassured.RestAssured.*;
+
+	import org.testng.Assert;
+
+	public class RestAssuredGitHubSteps {
+		
+		Response res;
+		
+		
+		@Given("User Enters github base URL and Authorization")
+		public void user_enters_github_base_url_and_authorization() {
+			
+			given().baseUri("https://api.github.com").basePath("/user/repos")
+			.header("Authorization","Bearer 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");
+			
+			
+		}
+		  
+		
+
+		@When("User executes HTTP get method")
+		public void user_executes_http_get_method() {
+			
+			res = given().baseUri("https://api.github.com").basePath("/user/repos")
+					.header("Authorization","Bearer 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")
+					.when().get();
+		 
+		}
+
+		@Then("Validate the response status code")
+		public void validate_the_response_status_code() {
+			
+			res.prettyPrint();
+			Assert.assertEquals(res.statusCode(), 200);
+			
+		}
+		 
+
+	}
